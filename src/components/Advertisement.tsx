45dc8b6d8da95f26cd98f1f2308d475ffd07cd9c
nikachitava <chitava18.official@gmail.com>
2024-12-30 23:21:21 +0400

    create header adevritesement component

---
+++ src/components/Advertisement.tsx
@@ -0,0 +1,22 @@
+const Advertisement = () => {
+	return (
+		<div className="w-full bg-button">
+			<div className="container flex items-center">
+				<div className="flex flex-1 items-center justify-center py-4 gap-2">
+					<p className="text-text text-sm font-poppins text-center">
+						Summer Sale For All Swim Suits And Free Express Delivery
+						- OFF 50%!
+					</p>
+					<strong className="font-semibold font-poppins text-text underline">
+						ShopNow
+					</strong>
+				</div>
+				<div>
+					<p className="text-text">ENGLISH</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Advertisement;
