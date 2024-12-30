cd96fe696083273d0f659460bb5778ea0147a446
nikachitava <chitava18.official@gmail.com>
2024-12-30 22:57:05 +0400

    setup colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#FFFFFF",
+				primary1: "#363738",
+				seconday: "#F5F5F5",
+				secondary1: "#FEFAF1",
+				secondary2: "#DB4444",
+				bg: "#FFFFFF",
+				text: "#FAFAFA",
+				text1: "#7D8184",
+				text2: "#000000",
+				button: "#000000",
+				button1: "#00FF66",
+				button2: "#DB4444",
+				hoverButtonRed: "#E07575",
+				hoverButtonBlue: "#A0BCE0",
+			},
+		},
 	},
 	plugins: [],
 };
