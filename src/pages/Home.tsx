0eb3c144f0f7605d9e8defe75e260fc91e20a6db
nikachitava <chitava18.official@gmail.com>
2024-12-31 22:42:15 +0400

    implement header in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
+import Header from "../components/Header";
 import SideMenuList from "../components/SideMenuList";
 
 const Home = () => {
 	return (
-		<div className="container flex items-start justify-between gap-28 py-10">
-			<SideMenuList />
-			<img
-				src="/images/main_banner.svg"
-				alt="main_banner"
-				className="flex-1"
-				loading="lazy"
-			/>
+		<div className="container">
+			<section className="flex items-start justify-between gap-28 py-10">
+				<SideMenuList />
+				<img
+					src="/images/main_banner.svg"
+					alt="main_banner"
+					className="flex-1"
+					loading="lazy"
+				/>
+			</section>
+			<Header title="Today's" />
 		</div>
 	);
 };
