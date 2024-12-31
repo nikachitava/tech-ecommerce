197a0c824612c811fa2d324ea909d914627578d6
nikachitava <chitava18.official@gmail.com>
2024-12-31 17:08:47 +0400

    create active menu list

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,35 @@
 import SearchInput from "./SearchInput";
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
+	const location = useLocation();
+
+	const menuList = [
+		{ name: "Home", path: "/" },
+		{ name: "Contact", path: "/contact" },
+		{ name: "About", path: "/about" },
+		{ name: "Sign up", path: "/auth" },
+	];
+
 	return (
 		<div className="border-b border-button">
 			<header className="container flex items-center justify-between pt-10 pb-4">
 				<h1 className="font-inter text-2xl font-bold ">Exclusive</h1>
 				<nav className="flex">
 					<ul className="flex items-center gap-12">
-						<li className="font-poppins text-text2">Home</li>
-						<li className="font-poppins text-text2">Contact</li>
-						<li className="font-poppins text-text2">About</li>
-						<li className="font-poppins text-text2">Sing Up</li>
+						{menuList.map((item) => (
+							<Link to={`${item.path}`}>
+								<li
+									className={`menu_list_item ${
+										location.pathname === item.path
+											? "underline font-semibold underline-offset-4"
+											: null
+									}`}
+								>
+									{item.name}
+								</li>
+							</Link>
+						))}
 					</ul>
 				</nav>
 				<div className="flex items-center gap-6">
