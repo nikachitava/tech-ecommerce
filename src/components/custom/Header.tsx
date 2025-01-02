1adbed573c7390e7fc784331de19071c99e5416f
nikachitava <chitava18.official@gmail.com>
2025-01-02 23:35:39 +0400

    implement timer component

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,3 +1,5 @@
+import Timer from "./Timer";
+
 interface HeaderProps {
 	title: string;
 	header: string;
@@ -6,11 +8,14 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ title, header }) => {
 	return (
 		<div className="space-y-5">
-			<div className="flex items-center gap-4">
-				<div className="bg-secondary2 w-5 h-10 rounded"></div>
-				<h4 className="text-secondary2 font-semibold font-poppins">
-					{title}
-				</h4>
+			<div className="flex justify-between items-center">
+				<div className="flex items-center gap-4">
+					<div className="bg-secondary2 w-5 h-10 rounded"></div>
+					<h4 className="text-secondary2 font-semibold font-poppins">
+						{title}
+					</h4>
+				</div>
+				<Timer />
 			</div>
 			<h1 className="text-4xl font-poppins text-text2 font-semibold">
 				{header}
