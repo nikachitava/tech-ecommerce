1f7ff16257ac0968be73c6f68f389373e7afb5af
nikachitava <chitava18.official@gmail.com>
2025-01-02 22:50:51 +0400

    add main title on header component

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,14 +1,20 @@
 interface HeaderProps {
 	title: string;
+	header: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ title }) => {
+const Header: React.FC<HeaderProps> = ({ title, header }) => {
 	return (
-		<div className="flex items-center gap-4">
-			<div className="bg-secondary2 w-5 h-10 rounded"></div>
-			<h4 className="text-secondary2 font-semibold font-poppins">
-				{title}
-			</h4>
+		<div className="space-y-5">
+			<div className="flex items-center gap-4">
+				<div className="bg-secondary2 w-5 h-10 rounded"></div>
+				<h4 className="text-secondary2 font-semibold font-poppins">
+					{title}
+				</h4>
+			</div>
+			<h1 className="text-4xl font-poppins text-text2 font-semibold">
+				{header}
+			</h1>
 		</div>
 	);
 };
