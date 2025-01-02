3d9f323f29f64e1128809f751b8676065a27d75a
nikachitava <chitava18.official@gmail.com>
2025-01-02 23:17:18 +0400

    change words for translating

--- src/sections/CategorySection.tsx
+++ src/sections/CategorySection.tsx
@@ -7,24 +7,30 @@ import smartwachIcon from "/icons/smartwatch.svg";
 import cameraIcon from "/icons/camera.svg";
 import headphoneIcon from "/icons/headphone.svg";
 import gamingIcon from "/icons/gaming.svg";
+import { useTranslation } from "react-i18next";
 
 const CategorySection = () => {
 	const categoriesList = [
-		{ icon: phoneIcon, title: "Phone" },
-		{ icon: computersIcon, title: "Computers" },
-		{ icon: smartwachIcon, title: "Smartwatch" },
-		{ icon: cameraIcon, title: "Camera" },
-		{ icon: headphoneIcon, title: "Headphone" },
-		{ icon: gamingIcon, title: "Gaming" },
+		{ icon: phoneIcon, transLationKey: "Phones" },
+		{ icon: computersIcon, transLationKey: "Computers" },
+		{ icon: smartwachIcon, transLationKey: "Smartwatches" },
+		{ icon: cameraIcon, transLationKey: "Camera" },
+		{ icon: headphoneIcon, transLationKey: "Headphone" },
+		{ icon: gamingIcon, transLationKey: "Gaming" },
 	];
+
+	const { t } = useTranslation();
 
 	return (
 		<section className="space-y-16">
 			<Header title="Categories" header="Browse By Category" />
-			{/* <div className="flex flex-wrap items-center justify-between gap-5 lg:gap-10"> */}
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-10">
-				{categoriesList.map(({ icon, title }) => (
-					<CategoryCard key={icon} icon={icon} title={title} />
+				{categoriesList.map(({ icon, transLationKey }) => (
+					<CategoryCard
+						key={icon}
+						icon={icon}
+						title={t(transLationKey)}
+					/>
 				))}
 			</div>
 		</section>
