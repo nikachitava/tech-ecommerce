6a30816d40d508c9d4fa95ad61cdc54d250ddcac
nikachitava <chitava18.official@gmail.com>
2025-01-05 16:08:04 +0400

    add words for translating and implement them

--- src/sections/BestSellingProductsSection.tsx
+++ src/sections/BestSellingProductsSection.tsx
@@ -1,10 +1,13 @@
 import Header from "@/components/custom/Header";
 import BestSellingProductsContainer from "@/containers/BestSellingProductsContainer";
+import { useTranslation } from "react-i18next";
 
 const BestSellingProductsSection = () => {
+	const { t } = useTranslation();
+
 	return (
 		<section className="space-y-16">
-			<Header header="Best Selling Product" title="This Month" />
+			<Header header={t("BestSellingProduct")} title={t("ThisMonth")} />
 			<BestSellingProductsContainer />
 		</section>
 	);
