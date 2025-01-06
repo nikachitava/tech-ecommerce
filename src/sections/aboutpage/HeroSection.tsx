a2138889f6be704467db57111423015c66fc8f95
nikachitava <chitava18.official@gmail.com>
2025-01-06 14:55:37 +0400

    add words to translate and implement them about herosection

--- src/sections/aboutpage/HeroSection.tsx
+++ src/sections/aboutpage/HeroSection.tsx
@@ -1,23 +1,20 @@
+import { useTranslation } from "react-i18next";
+
 const HeroSection = () => {
+	const { t } = useTranslation();
+
 	return (
 		<div className="flex flex-col-reverse items-center gap-2 md:flex-row md:gap-20 lg:justify-between">
 			<div className="flex flex-col items-start py-36 space-y-10 break-words w-1/2">
 				<h1 className="font-poppins font-medium text-text2 text-5xl">
-					Our Story
+					{t("OurStory")}
 				</h1>
 				<div className="space-y-6">
 					<p className="font-poppins text-text2">
-						Launced in 2015, Exclusive is South Asia's premier
-						online shopping makterplace with an active presense in
-						Bangladesh. Supported by wide range of tailored
-						marketing, data and service solutions, Exclusive has
-						10,500 sallers and 300 brands and serves 3 millioons
-						customers across the region.
+						{t("OurStoryText1")}
 					</p>
 					<p className="font-poppins text-text2">
-						Exclusive has more than 1 Million products to offer,
-						growing at a very fast. Exclusive offers a diverse
-						assotment in categories ranging from consumer.
+						{t("OurStoryText2")}
 					</p>
 				</div>
 			</div>
