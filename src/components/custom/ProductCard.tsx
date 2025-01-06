5abdb523d37b4e92e2d6e0a3f4e735c58f0f295a
nikachitava <chitava18.official@gmail.com>
2025-01-06 21:31:08 +0400

    saving wishlist item's id in the localstorage

--- src/components/custom/ProductCard.tsx
+++ src/components/custom/ProductCard.tsx
@@ -1,8 +1,10 @@
 import heart from "/icons/heart.svg";
 import eye from "/icons/eye.svg";
 import { IProductCardProps } from "@/types/IProductCardProps";
+import { useWishList } from "@/states/wishListStore";
 
 const ProductCard: React.FC<IProductCardProps> = ({
+	id,
 	title,
 	discountPercentage,
 	price,
@@ -12,6 +14,8 @@ const ProductCard: React.FC<IProductCardProps> = ({
 	const discountPrice = price - (price * discountPercentage) / 100;
 	const roundedPrice = discountPrice.toFixed(2);
 	const roundedFloatPrice = parseFloat(roundedPrice);
+
+	const { addToWishList, wishList } = useWishList((state) => state);
 
 	return (
 		<div className="group min-w-[270px] shadow-lg cursor-pointer rounded group">
@@ -25,7 +29,14 @@ const ProductCard: React.FC<IProductCardProps> = ({
 				)}
 				<img src={thumbnail} alt={thumbnail} className="" />
 				<div className="absolute right-[12px] top-0">
-					<img src={heart} alt="ss" />
+					<img
+						src={heart}
+						alt="ss"
+						onClick={() => {
+							addToWishList(id);
+							console.log(wishList);
+						}}
+					/>
 					<img src={eye} alt="ss" />
 				</div>
 				<div className="absolute bg-black bottom-0 left-0 w-full translate-y-full group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-1000">
--- src/states/wishListStore.ts
+++ src/states/wishListStore.ts
@@ -1,18 +1,31 @@
-import { Product } from '@/types/ProductType';
-import { create } from 'zustand'
+import { create } from "zustand";
 
 interface wishListState {
-    wishCount: number,
-    wishesList: any;
-    addToWishList: (item: Product) => void;
-    removeFromWishList: (id: number) => void;
+	wishCount: number;
+	wishList: any;
+	addToWishList: (id: number) => void;
+	removeFromWishList: (id: number) => void;
 }
 
-export const useWishList = create<wishListState>(() => ({
-    wishCount: 0,
-    wishesList: [],
-    addToWishList: (item) => {
-        console.log(item)
-    },
-    removeFromWishList: () => {}
-}))
+export const useWishList = create<wishListState>((set) => ({
+	wishCount: JSON.parse(localStorage.getItem("wishList") || "[]").length,
+	wishList: JSON.parse(localStorage.getItem("wishList") || "[]"),
+	addToWishList: (id) =>
+		set((state) => {
+			const updatedWishList = state.wishList.includes(id)
+				? state.wishList.filter((wishId: number) => wishId !== id)
+				: [...state.wishList, id];
+
+			localStorage.setItem("wishList", JSON.stringify(updatedWishList));
+			return { wishList: updatedWishList, wishCount: updatedWishList.length };
+		}),
+	removeFromWishList: (id) =>
+		set((state) => {
+			const updatedWishList = state.wishList.filter(
+				(wishId: number) => wishId !== id
+			);
+
+			localStorage.setItem("wishList", JSON.stringify(updatedWishList)); 
+			return { wishList: updatedWishList, wishCount: updatedWishList.length };
+		}),
+}));
