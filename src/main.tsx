c1bc417793cd8e8efa9854b03d85e3c0ff65eb03
nikachitava <chitava18.official@gmail.com>
2025-01-06 18:44:42 +0400

    remove redux wrapper from entry main component

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,9 @@ import AppRoutes from "./routes/AppRoutes";
 import "./utils/i18n";
 
 import "./index.css";
-import { store } from "./state/store";
-import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<Provider store={store}>
-			<AppRoutes />
-		</Provider>
+		<AppRoutes />
 	</StrictMode>
 );
