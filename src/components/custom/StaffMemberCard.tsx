028c3ec0ebe05a073091cf1db6a6773d851f68bf
nikachitava <chitava18.official@gmail.com>
2025-01-06 14:37:51 +0400

    modify staff member card

--- src/components/custom/StaffMemberCard.tsx
+++ src/components/custom/StaffMemberCard.tsx
@@ -1,34 +1,43 @@
-import emma_watson from "/icons/emma_watson.svg";
 import linkedin_icon from "/icons/linkedin_icon.svg";
 import instagram_icon from "/icons/instagram_icon.svg";
+import { IStaffMemberDataType } from "@/types/StaffMemberDataType";
 
-const StaffMemberCard = () => {
+const StaffMemberCard: React.FC<IStaffMemberDataType> = ({
+	image,
+	name,
+	role,
+	socialmedia,
+}) => {
 	return (
 		<div className="min-w-[370px] shadow-md">
 			<div className="bg-secondary2 flex items-center justify-center">
-				<img src={emma_watson} alt="staff_member" loading="lazy" />
+				<img src={image} alt="staff_member" loading="lazy" />
 			</div>
 			<div className="space-y-4 p-1">
 				<div className="mt-8">
 					<h1 className="font-poppins text-3xl text-text2 font-medium">
-						Emma Watson
+						{name}
 					</h1>
-					<h4 className="font-poppins text-text2">
-						Founder & Chairman
-					</h4>
+					<h4 className="font-poppins text-text2">{role}</h4>
 				</div>
 				<div className="bg-secondary1 flex items-center gap-4">
 					<img
 						src={instagram_icon}
 						alt="instagram_icon"
 						loading="lazy"
-						className="brightness-0"
+						className="brightness-0 cursor-pointer"
+						onClick={() => {
+							window.open(socialmedia.instagram, "_blank");
+						}}
 					/>
 					<img
 						src={linkedin_icon}
 						alt="linkedin_icon"
 						loading="lazy"
-						className="brightness-0"
+						className="brightness-0 cursor-pointer"
+						onClick={() => {
+							window.open(socialmedia.instagram, "_blank");
+						}}
 					/>
 				</div>
 			</div>
