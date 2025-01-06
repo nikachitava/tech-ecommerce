4c431471c9dc041abb0d7ad27f701757d7449484
nikachitava <chitava18.official@gmail.com>
2025-01-06 21:57:13 +0400

    start setup WishlistPage design

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,5 +1,27 @@
+import CustomButton from "@/components/custom/CustomButton";
+import { useWishList } from "@/states/wishListStore";
+
 const Wishlist = () => {
-	return <div>Wishlist</div>;
+	const { wishCount } = useWishList((state) => state);
+
+	return (
+		<section className="container my-20 space-y-16">
+			<div className="flex items-center justify-between">
+				<h1 className="font-poppins font-medium text-xl">
+					Wishlist ({wishCount})
+				</h1>
+				<CustomButton
+					title="Move All To Bag"
+					otherStyles="py-4 px-12 border border-[#000000] rounded"
+					filled={false}
+					onClick={() => {}}
+				/>
+			</div>
+			<div>
+				<h1>ssdadasdqw</h1>
+			</div>
+		</section>
+	);
 };
 
 export default Wishlist;
