c7c54c90407d01234eb32e3484bdeec70932c844
nikachitava <chitava18.official@gmail.com>
2025-01-06 18:54:11 +0400

    add key on map

--- src/sections/homepage/ServiceSection.tsx
+++ src/sections/homepage/ServiceSection.tsx
@@ -6,8 +6,9 @@ const ServiceSection = () => {
 	const { t } = useTranslation();
 	return (
 		<div className="flex flex-col items-center gap-8 md:flex-row md:justify-around">
-			{serviceList.map((service) => (
+			{serviceList.map((service, index) => (
 				<ServiceCard
+					key={index}
 					image={service.image}
 					title={t(service.titleTranslationKey)}
 					description={t(service.descriptionTranslationKey)}
