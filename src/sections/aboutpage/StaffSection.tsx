2973877fae4b34a9ee86c9194407dad9f28d517b
nikachitava <chitava18.official@gmail.com>
2025-01-06 14:38:14 +0400

    map staffmemberslistdata and implement it on section

--- src/sections/aboutpage/StaffSection.tsx
+++ src/sections/aboutpage/StaffSection.tsx
@@ -1,11 +1,18 @@
 import StaffMemberCard from "@/components/custom/StaffMemberCard";
+import { staffMembersDataList } from "@/data/StaffMembersData";
 
 const StaffSection = () => {
 	return (
 		<div className="flex flex-col gap-10 md:flex-row lg:flex-wrap lg:justify-between">
-			<StaffMemberCard />
-			<StaffMemberCard />
-			<StaffMemberCard />
+			{staffMembersDataList.map((member) => (
+				<StaffMemberCard
+					key={member.id}
+					image={member.image}
+					name={member.name}
+					role={member.role}
+					socialmedia={member.socialmedia}
+				/>
+			))}
 		</div>
 	);
 };
