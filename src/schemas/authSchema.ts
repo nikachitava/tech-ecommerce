105bee55b35c6a96f492b2061e235426b6e44976
nikachitava <chitava18.official@gmail.com>
2025-01-07 13:40:20 +0400

    add confirm passowrd validation to schema

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,6 +1,17 @@
-import { z } from 'zod'
+import { z } from "zod";
 
-export const authSchema = z.object({
-    email: z.string().min(2).max(50),
-    password: z.string().min(2).max(50),
-});
+export const authSchema = z
+	.object({
+		email: z
+			.string()
+			.min(1, { message: "This field has to be filled." })
+			.email("This is not a valid email."),
+		password: z.string().min(2).max(50),
+		confirm_password: z
+			.string()
+			.min(1, { message: "Please confirm your password." }),
+	})
+	.refine((data) => data.password === data.confirm_password, {
+		message: "Passwords do not match.",
+		path: ["confirm_password"],
+	});
