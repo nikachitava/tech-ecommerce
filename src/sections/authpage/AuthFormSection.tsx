73b252b272d3d16611c9f0b36ea250e4326d7951
nikachitava <chitava18.official@gmail.com>
2025-01-07 13:42:10 +0400

    implement create account function

--- src/sections/authpage/AuthFormSection.tsx
+++ src/sections/authpage/AuthFormSection.tsx
@@ -5,6 +5,8 @@ import { Form } from "@/components/ui/form";
 import CustomInput from "@/components/custom/CustomInput";
 import { useTranslation } from "react-i18next";
 import { authSchema } from "@/schemas/authSchema";
+import { useAuth } from "@/states/authStore";
+import Loader from "@/components/custom/Loader";
 
 const AuthFormSection = () => {
 	const form = useForm<z.infer<typeof authSchema>>({
@@ -12,27 +14,40 @@ const AuthFormSection = () => {
 		defaultValues: {
 			email: "",
 			password: "",
+			confirm_password: "",
 		},
 	});
 
-	function onSubmit(values: z.infer<typeof authSchema>) {
-		console.log(values);
-	}
+	const {
+		formState: { isSubmitting },
+	} = form;
 
 	const { t } = useTranslation();
+
+	const { singUpEmailAndPassowrd } = useAuth((state) => state);
+
+	const handleSignInWithGoogle = async (
+		values: z.infer<typeof authSchema>
+	) => {
+		try {
+			await singUpEmailAndPassowrd(values.email, values.password);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	return (
 		<div className="mx-auto max-w-xs">
 			<Form {...form}>
 				<form
-					onSubmit={form.handleSubmit(onSubmit)}
+					onSubmit={form.handleSubmit(handleSignInWithGoogle)}
 					className="space-y-8"
 				>
 					<CustomInput
 						control={form.control}
 						name="email"
 						label={t("Email")}
-						placeholder={t("Password")}
+						placeholder={t("Email")}
 						styles="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none"
 					/>
 					<CustomInput
@@ -43,20 +58,34 @@ const AuthFormSection = () => {
 						placeholder={t("Password")}
 						styles="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none"
 					/>
+					<CustomInput
+						control={form.control}
+						name="confirm_password"
+						label={t("ConfirmPassword")}
+						type="password"
+						placeholder={t("Password")}
+						styles="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none"
+					/>
 					<button className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-						<svg
-							className="w-6 h-6 -ml-2"
-							fill="none"
-							stroke="currentColor"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-						>
-							<path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-							<circle cx="8.5" cy="7" r="4" />
-							<path d="M20 8v6M23 11h-6" />
-						</svg>
-						<span className="ml-3">{t("SignUp")}</span>
+						{isSubmitting ? (
+							<Loader />
+						) : (
+							<>
+								<svg
+									className="w-6 h-6 -ml-2"
+									fill="none"
+									stroke="currentColor"
+									strokeWidth="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+								>
+									<path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+									<circle cx="8.5" cy="7" r="4" />
+									<path d="M20 8v6M23 11h-6" />
+								</svg>
+								<span className="ml-3">{t("SignUp")}</span>
+							</>
+						)}
 					</button>
 				</form>
 			</Form>
