bf9b88cc896f9326cb2aab04b6e3d32d67e0c1e6
nikachitava <chitava18.official@gmail.com>
2025-01-08 19:13:14 +0400

    setup layoutfor admin dashboard

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,10 +1,25 @@
+import AdminNavBar from "@/components/custom/AdminNavBar";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 const DashboardLayout = () => {
+	const [isCollapsed, setIsCollapsed] = useState(false);
+
 	return (
-		<div>
-			<h1>EVERYWHERE</h1>
-			<Outlet />
+		<div className="flex min-h-screen bg-gray-50">
+			<AdminNavBar
+				isCollapsed={isCollapsed}
+				setIsCollapsed={setIsCollapsed}
+			/>
+			<main
+				className={`flex-1 transition-all duration-300 ${
+					isCollapsed ? "ml-20" : "ml-64"
+				}`}
+			>
+				<div className="p-8">
+					<Outlet />
+				</div>
+			</main>
 		</div>
 	);
 };
