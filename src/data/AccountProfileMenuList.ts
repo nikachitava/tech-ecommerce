5d9659869ce845aa3cc1cac40f20faf0ac2b7e76
nikachitava <chitava18.official@gmail.com>
2025-01-08 14:58:27 +0400

    create account profile menu list

---
+++ src/data/AccountProfileMenuList.ts
@@ -0,0 +1,14 @@
+export const menuItems = {
+    account: {
+        title: "Manage My Account",
+        items: ["My Profile", "Address Book", "My Payment Options"],
+    },
+    orders: {
+        title: "My Orders",
+        items: ["My Returns", "My Cancellations"],
+    },
+    wishlist: {
+        title: "My Wishlist",
+        items: [],
+    },
+};
