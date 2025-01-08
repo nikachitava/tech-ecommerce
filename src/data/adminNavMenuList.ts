2decde279ed0e689b07b9e8a593050e0353ac8e9
nikachitava <chitava18.official@gmail.com>
2025-01-08 19:12:13 +0400

    create menu list for admin dashboard menu

---
+++ src/data/adminNavMenuList.ts
@@ -0,0 +1,14 @@
+import {
+	Home,
+	LayoutDashboard,
+	Users,
+	Settings,
+} from "lucide-react";
+
+
+export const adminNavMenuList = [
+    { icon: Home, label: "Home", path: "/" },
+    { icon: LayoutDashboard, label: "Dashboard", path: "/dashboard" },
+    { icon: Users, label: "Users", path: "/users" },
+    { icon: Settings, label: "Settings", path: "/settings" },
+];
