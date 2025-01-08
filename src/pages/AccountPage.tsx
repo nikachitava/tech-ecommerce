1f403daf46665d183098851d41d4060dd3037184
nikachitava <chitava18.official@gmail.com>
2025-01-08 16:07:01 +0400

    implement menu on account page"

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,9 +1,14 @@
 import AccountProfileMenu from "@/components/custom/AccountProfileMenu";
+import EditProfileForm from "@/components/custom/EditProfileForm";
+import { useAccountProfileActiveItem } from "@/states/accountProfileMenuStore";
 
 const AccountPage = () => {
+	const { activeItem } = useAccountProfileActiveItem((state) => state);
+
 	return (
-		<section className="container my-20">
+		<section className="min-h-screen container my-20 flex">
 			<AccountProfileMenu />
+			{activeItem === "My Profile" && <EditProfileForm />}
 		</section>
 	);
 };
