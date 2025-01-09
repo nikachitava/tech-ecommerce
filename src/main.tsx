d14ae5bfdadab9e8cb3db3ba353898d2740cc2fd
nikachitava <chitava18.official@gmail.com>
2025-01-09 14:20:30 +0400

    set up tanstackquery client

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 import AppRoutes from "./routes/AppRoutes";
 
@@ -7,8 +8,12 @@ import "./utils/i18n";
 
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<AppRoutes />
+		<QueryClientProvider client={queryClient}>
+			<AppRoutes />
+		</QueryClientProvider>
 	</StrictMode>
 );
