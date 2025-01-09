adb6798482ece15efd14497cf9f9d5bef37204f7
nikachitava <chitava18.official@gmail.com>
2025-01-09 16:54:30 +0400

    crete category types

---
+++ src/types/CategoryType.ts
@@ -0,0 +1,20 @@
+interface Subcategory {
+	_id: string;
+	name: string;
+	slug: string;
+	description: string;
+	isActive: boolean;
+}
+
+export interface CategoryType {
+	_id: string;
+	name: string;
+	slug: string;
+	description: string;
+	image: string;
+	subcategories: Subcategory[];
+	isActive: boolean;
+	createdAt: string;
+	updatedAt: string;
+	__v: number;
+}
