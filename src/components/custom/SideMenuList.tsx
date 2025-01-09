2dc4be32735c87076a6271c82f2fffb6d5852ccd
nikachitava <chitava18.official@gmail.com>
2025-01-09 16:56:31 +0400

    get data using tanstack query from SideMenuListContainer

--- src/components/custom/SideMenuList.tsx
+++ src/components/custom/SideMenuList.tsx
@@ -1,15 +1,36 @@
-import { sideMenuList } from "../../data/SideMenuList";
 import SideMenuListItem from "./SideMenuListItem";
+import { ApiError } from "@/types/ApiRequest";
+import { CategoryType } from "@/types/CategoryType";
+import Loader from "./Loader";
 
-const SideMenuList = () => {
+interface ISideMenuListProps {
+	data?: CategoryType[];
+	isLoading: boolean;
+	error?: ApiError | null;
+}
+
+const SideMenuList: React.FC<ISideMenuListProps> = ({
+	data,
+	isLoading,
+	error,
+}) => {
+	if (isLoading) return <Loader />;
+	if (error) return <div>Error: {error.message}</div>;
+	if (!data || data.length === 0) {
+		return <div className="text-gray-500">No categories available</div>;
+	}
+
+	console.log("error: ", error);
+	console.log("data: ", console.log("error: ", error));
+
 	return (
 		<ul className="flex flex-col space-y-4 w-[220px]">
-			{sideMenuList.map(({ name, translationKey, arrow }) => (
+			{data?.map((item) => (
 				<SideMenuListItem
-					key={name}
-					name={name}
-					arrow={arrow}
-					translationKey={translationKey}
+					key={item._id}
+					name={item.name}
+					arrow={item.subcategories.length > 0}
+					translationKey={item.slug}
 				/>
 			))}
 		</ul>
