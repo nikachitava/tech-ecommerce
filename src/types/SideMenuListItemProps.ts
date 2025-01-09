980dfe6ec532d82364652a41a7561bfcc25dd511
nikachitava <chitava18.official@gmail.com>
2025-01-09 17:38:39 +0400

    add some properties

--- src/types/SideMenuListItemProps.ts
+++ src/types/SideMenuListItemProps.ts
@@ -1,5 +1,11 @@
+import { Subcategory } from "./CategoryType";
+
 export interface SideMenuListItemProps {
+    id: string;
     name: string;
     translationKey?: string;
-    arrow?: boolean
+    arrow?: boolean,
+    subcategories?: Subcategory[];
+    isOpen?: boolean,
+    onToggle: (id: string) => void;
 }
