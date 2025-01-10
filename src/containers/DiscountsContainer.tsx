66df201139d953278f54f8eb9dd53895c7365749
nikachitava <chitava18.official@gmail.com>
2025-01-10 15:40:14 +0400

    using tanstack query to fetch data

--- src/containers/DiscountsContainer.tsx
+++ src/containers/DiscountsContainer.tsx
@@ -1,28 +1,10 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Product } from "@/types/ProductType";
+import { useGetProductQueries } from "@/api/productQueries";
 import ProductList from "@/components/custom/ProductList";
 
 const DiscountsContainer = () => {
-	const [products, setProducts] = useState<Product[]>([]);
-	const [isLoading, setIsLoading] = useState(false);
+	const { data, isLoading, error } = useGetProductQueries();
 
-	const fetchProducts = async () => {
-		try {
-			setIsLoading(true);
-			const response = await axios.get("https://dummyjson.com/products");
-			setProducts(response.data.products);
-		} catch (error) {
-			console.log(error);
-		} finally {
-			setIsLoading(false);
-		}
-	};
-
-	useEffect(() => {
-		fetchProducts();
-	}, []);
-	return <ProductList products={products} isLoading={isLoading} />;
+	return <ProductList products={data} isLoading={isLoading} error={error} />;
 };
 
 export default DiscountsContainer;
