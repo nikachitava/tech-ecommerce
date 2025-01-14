48ff6c752848180cded04748c90e5057888e59ed
nikachitava <chitava18.official@gmail.com>
2025-01-14 22:39:06 +0400

    setup signup and sign in functions

--- src/states/authStore.ts
+++ src/states/authStore.ts
@@ -1,31 +1,66 @@
-import { create } from 'zustand'
+import { useAxios } from "@/hooks/useAxios";
+import { UserType } from "@/types/UserType";
+import { AxiosError, AxiosResponse } from "axios";
+import { create } from "zustand";
 
-interface User {
-    uid: string;
-    email: string | null;
-    displayName: string | null;
-    photoURL: string | null;
-}
+interface AuthError extends Error {
+    code?: string;
+    statusCode?: number;
+    message: string;
+  }
 
 interface authStoreTypes {
-    isAuth: boolean;
-    currentUser: User | null,
-    signUp: (name: string, lastname: string, emai: string, password: string) => Promise<void>;
-    signIn: (emai: string, password: string) => Promise<void>;
-    logout: () => Promise<void>;
+	isAuth: boolean;
+	currentUser: UserType | null;
+	signUp: (data: {
+		name: string;
+		lastname: string;
+		email: string;
+		password: string;
+	}) => Promise<AxiosResponse<any, any>>;
+	signIn: (data: {email: string, password: string}) => Promise<AxiosResponse<any, any>>;
+	logout: () => Promise<void>;
 }
 
-export const useAuth = create<authStoreTypes>(()=> ({
-    isAuth: false,
-    currentUser: null,
-    signUp: async () => {
-        console.log("sign up")
-    },
-    signIn: async () => {
-        console.log("sign in")
-    },
-    logout: async () => {
-        console.log("log out")
-    }
+export const useAuth = create<authStoreTypes>(() => ({
+	isAuth: false,
+	currentUser: null,
+	signUp: async (data) => {
+		try {
+			const response = await useAxios.post("/users/singup", data);
+			return response;
+		} catch (error) {
+			if (error instanceof AxiosError) {
+                const authError: AuthError = new Error(
+                  error.response?.data?.message || "Failed to sign up"
+                );
+                authError.code = error.code;
+                authError.statusCode = error.response?.status;
+                throw authError;
+              }
+              throw new Error("An unexpected error occurred");
+		}
+	},
+	signIn: async (data) => {
+		try {
+            const response = await useAxios.post("/users/auth", data);
+            console.log(response)
+            return response;
 
-}))
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                const authError: AuthError = new Error(
+                  error.response?.data?.message || "Failed to sign in"
+                );
+                authError.code = error.code;
+                authError.statusCode = error.response?.status;
+                throw authError;
+              }
+              throw new Error("An unexpected error occurred");
+        }
+	},
+	logout: async () => {
+		console.log("log out");
+	},
+}));
+1
