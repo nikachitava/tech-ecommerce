570496aa39619966bc4228401e58359dcbfa8e21
nikachitava <chitava18.official@gmail.com>
2025-01-14 21:33:44 +0400

    split auth and signup schemas and modify them

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -6,12 +6,25 @@ export const authSchema = z
 			.string()
 			.min(1, { message: "This field has to be filled." })
 			.email("This is not a valid email."),
-		password: z.string().min(2).max(50),
-		confirm_password: z
-			.string()
-			.min(1, { message: "Please confirm your password." }),
-	})
-	.refine((data) => data.password === data.confirm_password, {
-		message: "Passwords do not match.",
-		path: ["confirm_password"],
+		password: z.string().min(8).max(50),
 	});
+
+
+export const signUpSchema = z
+    .object({
+        name: z.string().min(1, {message: "This field is required"}),
+        lastname: z.string().min(1, {message: "This field is required"}),
+        email: z
+            .string()
+            .min(1, { message: "This field has to be filled." })
+            .email("This is not a valid email."),
+        password: z.string().min(2).max(50),
+        confirm_password: z
+            .string()
+            .min(1, { message: "Please confirm your password." }),
+    })
+    .refine((data) => data.password === data.confirm_password, {
+        message: "Passwords do not match.",
+        path: ["confirm_password"],
+    });
+    
