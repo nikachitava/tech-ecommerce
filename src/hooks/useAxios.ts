066d2777e51bf6e20e03b11bf45f755858784c0c
nikachitava <chitava18.official@gmail.com>
2025-01-17 17:32:13 +0400

    add axios interceptors

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,6 +1,21 @@
+import { useAuth } from '@/states/authStore';
 import axios from 'axios'
 
 export const useAxios = axios.create({
     baseURL: "http://localhost:3000/",
+    headers: {
+        'Content-Type': 'application/json'
+    },
     withCredentials: true
 })
+
+useAxios.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        if (error.response?.status === 401) {
+            // Clear auth state when token is invalid/expired
+            useAuth.getState().logout();
+        }
+        return Promise.reject(error);
+    }
+);
