171330e977c6a38322c7ec9bed1541241ffd7712
nikachitava <chitava18.official@gmail.com>
2025-01-19 17:06:26 +0400

    implenet remove item from wishlist on wishlist page

--- src/sections/wishlist/WishlistsSection.tsx
+++ src/sections/wishlist/WishlistsSection.tsx
@@ -1,5 +1,4 @@
 import { useGetProductsByIdQueries } from "@/api/productQueries";
-import Loader from "@/components/custom/Loader";
 import ProductCard from "@/components/custom/ProductCard";
 
 interface IWishlistSection {
@@ -7,15 +6,13 @@ interface IWishlistSection {
 }
 
 const WishlistsSection: React.FC<IWishlistSection> = ({ productIds }) => {
-	const { data, isLoading, error } = useGetProductsByIdQueries(productIds);
+	const { data, error } = useGetProductsByIdQueries(productIds);
 
-	if (isLoading) return <Loader />;
 	if (error) return <h1>{error.message}</h1>;
-	if (!data) return <h1 className="text-2xl">Wish list is empty</h1>;
 
 	return (
 		<section className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-			{data.map((item) => (
+			{data?.map((item) => (
 				<ProductCard
 					key={item._id}
 					id={item._id}
