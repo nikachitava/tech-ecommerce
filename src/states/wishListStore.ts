dcf60c98aadf94449d4f997c21fddd59be75ca21
nikachitava <chitava18.official@gmail.com>
2025-01-24 23:46:39 +0400

    save whole product instead of id

--- src/states/wishListStore.ts
+++ src/states/wishListStore.ts
@@ -1,38 +1,38 @@
+import { CartProductType } from "@/types/CartProductType";
 import { create } from "zustand";
 
-interface wishListState {
+interface WishListState {
 	wishCount: number;
-	wishList: any;
-	addToWishList: (id: string) => void;
+	wishList: CartProductType[];
+	addToWishList: (product: CartProductType) => void;
 	removeFromWishList: (id: string) => void;
-    clearWishList: () => void;
+	clearWishList: () => void;
 }
 
-export const useWishList = create<wishListState>((set) => ({
-	wishCount: JSON.parse(localStorage.getItem("wishList") || "[]").length,
-	wishList: JSON.parse(localStorage.getItem("wishList") || "[]"),
-	addToWishList: (id) =>
+export const useWishList = create<WishListState>((set) => ({
+	wishCount: 0,
+	wishList: [],
+	addToWishList: (product) =>
 		set((state) => {
-			const updatedWishList = state.wishList.includes(id)
-				? state.wishList.filter((wishId: string) => wishId !== id)
-				: [...state.wishList, id];
+			const exists = state.wishList.some((item) => item.id === product.id);
+			const updatedWishList = exists
+				? state.wishList.filter((item) => item.id !== product.id)
+				: [...state.wishList, product];
 
 			localStorage.setItem("wishList", JSON.stringify(updatedWishList));
 			return { wishList: updatedWishList, wishCount: updatedWishList.length };
 		}),
 	removeFromWishList: (id) =>
 		set((state) => {
-			const updatedWishList = state.wishList.filter(
-				(wishId: string) => wishId !== id
-			);
+			const updatedWishList = state.wishList.filter((item) => item.id !== id);
 
-			localStorage.setItem("wishList", JSON.stringify(updatedWishList)); 
+			localStorage.setItem("wishList", JSON.stringify(updatedWishList));
 			return { wishList: updatedWishList, wishCount: updatedWishList.length };
 		}),
-    clearWishList: ()=> 
-        set(() => {
+	clearWishList: () =>
+		set(() => {
 			localStorage.removeItem("wishList");
 			return { wishList: [], wishCount: 0 };
 		}),
-    
 }));
+
