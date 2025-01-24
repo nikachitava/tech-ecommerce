bcf45e81809788ce0c238dd867dbe63c99dde019
nikachitava <chitava18.official@gmail.com>
2025-01-24 23:30:37 +0400

    create cartSection

---
+++ src/sections/CartItemsSection.tsx
@@ -0,0 +1,29 @@
+import { useGetProductsByIdQueries } from "@/api/productQueries";
+import CartItem from "@/pages/CartItem";
+import { CartProductType } from "@/states/cartStore";
+
+interface ICartItemsSectionProps {
+	products: CartProductType[];
+}
+
+const CartItemsSection: React.FC<ICartItemsSectionProps> = ({ products }) => {
+	const { data, error } = useGetProductsByIdQueries(products);
+
+	if (error) return <h1>{error.message}</h1>;
+
+	return (
+		<div className="space-y-16">
+			{data?.map((item) => (
+				<CartItem
+					key={item._id}
+					id={item._id}
+					image={item.thumbnail}
+					price={item.price}
+					title={item.name}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default CartItemsSection;
