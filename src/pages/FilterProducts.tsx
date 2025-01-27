7572b8604a43556f0bdfaa707d463e8c3feb1ec1
nikachitava <chitava18.official@gmail.com>
2025-01-27 21:18:37 +0400

    filter products by price and category

--- src/pages/FilterProducts.tsx
+++ src/pages/FilterProducts.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import FilterSidebar from "@/components/custom/FilterMenu/FilterSideBar";
 import { useGetProductQueries } from "@/api/productQueries";
 import ProductCard from "@/components/custom/ProductCard";
@@ -8,24 +7,20 @@ import { useGetCategoriesQuery } from "@/api/categoryQueries";
 import { useFilter } from "@/states/filterStore";
 
 const FilterProducts = () => {
-	const { priceRange, setPriceRange } = useFilter((state) => state);
+	const { priceRange, setPriceRange, categories, setCategories } = useFilter(
+		(state) => state
+	);
 
-	const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-
-	const handleCategoryChange = (categoryId: string) => {
-		setSelectedCategories((prev) =>
-			prev.includes(categoryId)
-				? prev.filter((id) => id !== categoryId)
-				: [...prev, categoryId]
-		);
-	};
-
-	const categories = useGetCategoriesQuery();
+	const { data: categoriesData } = useGetCategoriesQuery();
 	const { data, isLoading } = useGetProductQueries();
 
-	const filterdData = data?.filter(
-		(item) => item.price > priceRange[0] && item.price < priceRange[1]
-	);
+	const filteredData = data?.filter((item) => {
+		const isWithinPriceRange =
+			item.price > priceRange[0] && item.price < priceRange[1];
+		const isInSelectedCategories =
+			categories.length === 0 || categories.includes(item.category);
+		return isWithinPriceRange && isInSelectedCategories;
+	});
 
 	if (isLoading) return <Loader />;
 
@@ -34,15 +29,15 @@ const FilterProducts = () => {
 			<FilterSidebar
 				priceRange={priceRange}
 				onPriceChange={setPriceRange}
-				categories={categories.data}
-				selectedCategories={selectedCategories}
-				onCategoryChange={handleCategoryChange}
+				categories={categoriesData}
+				selectedCategories={categories}
+				onCategoryChange={setCategories}
 			/>
 			<div className="pl-72">
 				<div className="p-8">
 					<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-						{filterdData &&
-							filterdData.map((product) => (
+						{filteredData &&
+							filteredData.map((product) => (
 								<Link
 									to={`/product/${product._id}`}
 									key={product._id}
@@ -62,49 +57,6 @@ const FilterProducts = () => {
 				</div>
 			</div>
 		</div>
-
-		// <div className="min-h-screen bg-gradient-to-br from-white to-filter-light/30">
-		// 	<div className="container mx-auto px-4 py-8">
-		// 		<div className="flex justify-between items-center mb-8">
-		// 			<Button
-		// 				onClick={() => setIsFilterOpen(true)}
-		// 				variant="outline"
-		// 				className="flex items-center gap-2 border-filter-primary/20 hover:bg-filter-primary/5 hover:border-filter-primary transition-colors duration-200"
-		// 			>
-		// 				<Filter className="h-4 w-4 text-filter-primary" />
-		// 				<span className="text-filter-text">Filters</span>
-		// 			</Button>
-		// 		</div>
-
-		// 		<FilterSidebar
-		// 			isOpen={isFilterOpen}
-		// 			onClose={() => setIsFilterOpen(false)}
-		// 			priceRange={priceRange}
-		// 			onPriceChange={setPriceRange}
-		// 			categories={categories.data}
-		// 			selectedCategories={selectedCategories}
-		// 			onCategoryChange={handleCategoryChange}
-		// 		/>
-
-		// 		<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-		// 			{filterdData &&
-		// 				filterdData.map((product) => (
-		// 					<Link
-		// 						to={`/product/${product._id}`}
-		// 						key={product._id}
-		// 					>
-		// 						<ProductCard
-		// 							id={product._id}
-		// 							discountPercent={product.discountPercent}
-		// 							price={product.price}
-		// 							thumbnail={product.thumbnail}
-		// 							title={product.name}
-		// 						/>
-		// 					</Link>
-		// 				))}
-		// 		</div>
-		// 	</div>
-		// </div>
 	);
 };
 
